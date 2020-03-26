36238c78d61fdb2f526c69292f9bde08cd9b12d6
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-03-26 16:13:27 +0900

    score
    이름과 성적을 입력하면 그에 맞는 학점이 출력

---
+++ score.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+
+    println("이름 : 안창완")
+    println("학번 : 1501023")
+
+
+    println("이름을 입력하세요")
+    var name:String= readLine()!!
+    println("성적을 입력하세요")
+    var score:Int= readLine()!!.toInt()
+
+
+    if(score>=90){
+        println("성적 : A")
+    }
+    if(score<90&&score>=80){
+        println("성적 : B")
+    }
+    if(score<80&&score>=70){
+        println("성적 : C")
+    }
+    if(score<70&&score>=60){
+        println("성적 : D")
+    }
+    else if(score<60){
+        println("성적 : F")
+    }
+}
