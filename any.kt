dd3f604e408b76d1d914c7ca18470bc864839df9
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-03-27 22:05:25 +0900

    Update any.kt

--- any.kt
+++ any.kt
@@ -5,15 +5,18 @@ fun main(args: Array<String>) {
     everybody="문자열 테스트"
     everybody=221.01010
     everybody=12.00f
+    
+    //is !(not) 연산자로 데이터 형을 확인해준다
     if(everybody !is String){
         if(everybody is Float){
             println("float입니다.")
         }
     }
 
-
+    //2. null을 표시하고 싶을때는 any?이렇게 해주면 된다.
     //everybody = null
-
+    
+    //3. 값을 비교하기
     println(everybody ==12.00f)
     println(everybody.equals(12.00f))
 
