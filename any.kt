c4281c4d9fbb6c3cbb5847a4e90027583a086e9e
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-03-27 22:03:35 +0900

    any형
    any형은 문자열을 처리하는 함수
    - 어떠한 형을  대입시켜도 처리가능한  변수형
    - 최종대입된 값으로 형이 결정된다.
    - null을 엄격히 구분한다.->null을 허용하는  변수는? 정의

---
+++ any.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var everybody:Any
+
+    everybody=1111
+    everybody="문자열 테스트"
+    everybody=221.01010
+    everybody=12.00f
+    if(everybody !is String){
+        if(everybody is Float){
+            println("float입니다.")
+        }
+    }
+
+
+    //everybody = null
+
+    println(everybody ==12.00f)
+    println(everybody.equals(12.00f))
+
+    //Unit형은 값이 없음을 정의하는 형
+
+    // Nothing형은 미래가 없음을 알리는 형
+}
+fun
